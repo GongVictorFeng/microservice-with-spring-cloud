f0b713a76a778affcfa376e063b3380a34149edc
victor <gfeng005@uottawa.ca>
2025-05-16 14:00:40 -0400

    used Feign framework to simplify the process

--- currency-conversion-service/src/main/java/com/microservices/currency_conversion_service/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/microservices/currency_conversion_service/CurrencyConversionController.java
@@ -12,6 +12,12 @@ import java.util.HashMap;
 @RestController
 public class CurrencyConversionController {
 
+    private CurrencyExchangeProxy currencyExchangeProxy;
+
+    public CurrencyConversionController(CurrencyExchangeProxy currencyExchangeProxy) {
+        this.currencyExchangeProxy = currencyExchangeProxy;
+    }
+
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion (
             @PathVariable String from,
@@ -24,5 +30,15 @@ public class CurrencyConversionController {
         ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariable);
         CurrencyConversion currencyConversion = responseEntity.getBody();
         return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getConversionMultiple(), quantity, quantity.multiply(currencyConversion.getConversionMultiple()),currencyConversion.getEnvironment());
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign (
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getConversionMultiple(), quantity, quantity.multiply(currencyConversion.getConversionMultiple()),currencyConversion.getEnvironment() + " feign");
     }
 }

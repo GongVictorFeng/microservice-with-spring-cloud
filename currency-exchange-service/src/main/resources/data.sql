62bebec4dad010a497104dcb4b4cc67854da7b3b
victor <gfeng005@uottawa.ca>
2025-05-16 10:24:04 -0400

    set up environment to use h2 and jpa.

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','CNY',7.19,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','CNY',8.05,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'CAD','CNY',5.16,'');

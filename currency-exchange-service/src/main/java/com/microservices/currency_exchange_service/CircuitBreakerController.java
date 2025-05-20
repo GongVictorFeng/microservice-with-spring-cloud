78870a5da779acf04ffed4432bd96af1f9aaac3e
victor <gfeng005@uottawa.ca>
2025-05-20 19:33:54 -0400

    implemented retry for fault tolerance by resilience4j

---
+++ currency-exchange-service/src/main/java/com/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -0,0 +1,26 @@
+package com.microservices.currency_exchange_service;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+    @GetMapping("sample-api")
+    @Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
+    public String sampleApi() {
+        logger.info("Sample Api call received");
+        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+        return responseEntity.getBody();
+    }
+
+    public String hardcodedResponse(Exception ex) {
+        return "fallback-response";
+    }
+}
